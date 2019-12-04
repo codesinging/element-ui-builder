1cfa95aa4dfb7aa945555603ff36e9ae16f73fcb
codesinging <codesinging@gmail.com>
2019-12-04 18:49:03 +0800

    Add basic class ElementUi.

---
+++ src/ElementUi.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: CodeSinging <codesinging@gmail.com>
+ * Time: 2019/12/4 18:42
+ */
+
+namespace CodeSinging\ElementUiBuilder;
+
+use CodeSinging\ComponentBuilder\Component;
+
+class ElementUi extends Component
+{
+    protected $tagPrefix = 'el-';
+}
