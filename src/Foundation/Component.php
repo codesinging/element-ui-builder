2aaebcfc4d9bc88e70b698e043cfd45da69d7ab1
codesinging <codesinging@gmail.com>
2020-01-14 17:53:10 +0800

    Update Component: add compId and config methods.

--- src/Foundation/Component.php
+++ src/Foundation/Component.php
@@ -7,6 +7,7 @@
 namespace CodeSinging\ElementUiBuilder\Foundation;
 
 use CodeSinging\ComponentBuilder\Builder;
+use CodeSinging\Support\Repository;
 use CodeSinging\Support\Str;
 
 class Component extends Builder
@@ -24,6 +25,24 @@ class Component extends Builder
     protected $baseTag;
 
     /**
+     * The component id.
+     * @var int
+     */
+    protected $compId;
+
+    /**
+     * The component count.
+     * @var int
+     */
+    protected static $compCount = 0;
+
+    /**
+     * The component config instance.
+     * @var Repository
+     */
+    protected $config;
+
+    /**
      * Component constructor.
      *
      * @param array $attributes
@@ -31,6 +50,8 @@ class Component extends Builder
     public function __construct(array $attributes = [])
     {
         parent::__construct($this->tagPrefix . $this->baseTag(), null, $attributes, true, false);
+        $this->compId = ++self::$compCount;
+        $this->config = new Repository();
     }
 
     /**
@@ -42,4 +63,33 @@ class Component extends Builder
         return $this->baseTag ?: Str::kebab((basename(str_replace('\\', '/', get_class($this)))));
     }
 
+    /**
+     * Get the component id.
+     *
+     * @param string|null $prefix
+     *
+     * @return int|string
+     */
+    public function compId(string $prefix = null)
+    {
+        return $prefix ? ($prefix . '_' . $this->compId) : $this->compId;
+    }
+
+    /**
+     * Get or set component config.
+     *
+     * @param string|array $key
+     * @param mixed|null   $default
+     *
+     * @return $this|mixed
+     */
+    public function config($key, $default = null)
+    {
+        if (is_array($key)) {
+            $this->config->set($key);
+            return $this;
+        } else {
+            return $this->config->get($key, $default);
+        }
+    }
 }
