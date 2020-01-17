197dc37c6cd80fa15ddd170606546fb4e6b0dca2
codesinging <codesinging@gmail.com>
2020-01-17 17:41:46 +0800

    Refactor Form and FormItem.

--- src/Components/Form.php
+++ src/Components/Form.php
@@ -8,6 +8,7 @@ namespace CodeSinging\ElementUiBuilder\Components;
 
 use Closure;
 use CodeSinging\ElementUiBuilder\Foundation\Component;
+use CodeSinging\Support\Str;
 
 /**
  * Class Form
@@ -25,6 +26,30 @@ use CodeSinging\ElementUiBuilder\Foundation\Component;
  * @method $this validateOnRuleChange(bool $validateOnRuleChange = true, $store = null)
  * @method $this size(string $size, $store = null)
  * @method $this disabled(bool $disabled = true, $store = null)
+ *
+ * @method FormItem radioItem(string $prop, string $label, string|int|bool|Closure|Radio $radioLabel = null, string $radioContent = null, array $attributes = null)
+ * @method FormItem radioGroupItem(string $prop, string $label, array|Closure|RadioGroup $options = null, array $attributes = null)
+ * @method FormItem checkboxItem(string $prop, string $label, string|Closure|Checkbox $content = null, array $attributes = null)
+ * @method FormItem checkboxGroupItem(string $prop, string $label, array|Closure|CheckboxGroup $options = null, array $attributes = null)
+ * @method FormItem inputItem(string $prop, string $label, array|Closure|Input $attributes = null)
+ * @method FormItem inputNumberItem(string $prop, string $label, array|Closure|InputNumber $attributes = null)
+ * @method FormItem selectItem(string $prop, string $label, array|Closure|Select $options = null, array $attributes = null)
+ * @method FormItem cascaderItem(string $prop, string $label, array|Closure|Cascader $options = null, array $attributes = null)
+ * @method FormItem switcherItem(string $prop, string $label, array|Closure|Switcher $attributes = null)
+ * @method FormItem sliderItem(string $prop, string $label, array|Closure|Slider $attributes = null)
+ * @method FormItem datePickerItem(string $prop, string $label, array|Closure|DatePicker $attributes = null)
+ * @method FormItem dateTimePickerItem(string $prop, string $label, array|Closure|DateTimePicker $attributes = null)
+ * @method FormItem datesPickerItem(string $prop, string $label, array|Closure|DatesPicker $attributes = null)
+ * @method FormItem dateRangePickerItem(string $prop, string $label, array|Closure|DateRangePicker $attributes = null)
+ * @method FormItem dateTimeRangePickerItem(string $prop, string $label, array|Closure|DateTimeRangePicker $attributes = null)
+ * @method FormItem weekPickerItem(string $prop, string $label, array|Closure|WeekPicker $attributes = null)
+ * @method FormItem monthPickerItem(string $prop, string $label, array|Closure|MonthPicker $attributes = null)
+ * @method FormItem yearPickerItem(string $prop, string $label, array|Closure|YearPicker $attributes = null)
+ * @method FormItem timeSelectItem(string $prop, string $label, array|Closure|TimeSelect $attributes = null)
+ * @method FormItem timePickerItem(string $prop, string $label, array|Closure|TimePicker $attributes = null)
+ * @method FormItem rateItem(string $prop, string $label, array|Closure|Rate $attributes = null)
+ * @method FormItem colorPicker(string $prop, string $label, array|Closure|ColorPicker $attributes = null)
+ * @method FormItem transferItem(string $prop, string $label, string|array|Closure|Transfer $data = null, array $attributes = null)
  *
  * @package CodeSinging\ElementUiBuilder\Components
  */
@@ -46,7 +71,7 @@ class Form extends Component
      * @param string|array|null $model
      * @param array             $attributes
      */
-    public function __construct($model = null, array $attributes = [])
+    public function __construct($model = null, array $attributes = null)
     {
         if (is_array($model)) {
             parent::__construct($model);
@@ -67,7 +92,7 @@ class Form extends Component
      *
      * @return FormItem
      */
-    public function item($prop = null, string $label = null, array $attributes = [])
+    public function item($prop = null, string $label = null, array $attributes = null)
     {
         if ($prop instanceof Closure) {
             $item = new FormItem();
@@ -81,6 +106,59 @@ class Form extends Component
         $this->add($item);
 
         return $item;
+    }
+
+    /**
+     * Bind a component to the form item.
+     *
+     * @param string $name
+     * @param array  $arguments
+     *
+     * @return FormItem
+     */
+    protected function itemWithComponent(string $name, array $arguments)
+    {
+        $class = Str::beforeLast(get_class($this), '\\') . '\\' . $name;
+
+        $prop = array_shift($arguments);
+        $label = array_shift($arguments);
+        $param = array_shift($arguments);
+
+        /** @var Component $component */
+        $component = new $class();
+
+        if ($param instanceof Closure) {
+            $component = call_user_func($param, $component) ?? $component;
+        } elseif ($param instanceof Component) {
+            $component = $param;
+        } else {
+            $component = new $class(null, $param, ...$arguments);
+        }
+
+        $component->vModel($this->get('model') . '.' . $prop);
+
+        $item = $this->item($prop, $label)->add($component);
+
+        return $item;
+    }
+
+    /**
+     * Handle dynamic calls to the builder to set attributes or add a FormItem bound with a component.
+     *
+     * @param $name
+     * @param $arguments
+     *
+     * @return $this|FormItem
+     */
+    public function __call($name, $arguments)
+    {
+        if (Str::endsWith($name, 'Item')) {
+            $name = Str::studly(Str::beforeLast($name, 'Item'));
+            return $this->itemWithComponent($name, $arguments);
+        }
+
+        parent::__call($name, $arguments);
+        return $this;
     }
 
 }
--- src/Components/FormItem.php
+++ src/Components/FormItem.php
@@ -24,30 +24,6 @@ use CodeSinging\Support\Str;
  * @method $this inlineMessage(bool $inlineMessage = true, $store = null)
  * @method $this size(string $size, $store = null)
  *
- * @method FormItem radio($model = null, $label = null, string $content = null, array $props = [])
- * @method FormItem radioGroup($model, array $options = [], array $props = [])
- * @method FormItem checkbox($model = null, $label = null, string $content = null, array $props = [])
- * @method FormItem checkboxGroup($model, array $options = [], array $props = [])
- * @method FormItem input($model, array $props = [])
- * @method FormItem inputNumber($model, array $props = [])
- * @method FormItem select($model, array $options = [], array $props = [])
- * @method FormItem cascader($model, string $options = null, array $props = [])
- * @method FormItem switcher($model, array $props = [])
- * @method FormItem slider($model, array $props = [])
- * @method FormItem datePicker($model, array $props = [])
- * @method FormItem dateTimePicker($model, array $props = [])
- * @method FormItem datesPicker($model, array $props = [])
- * @method FormItem dateRangePicker($model, array $props = [])
- * @method FormItem dateTimeRangePicker($model, array $props = [])
- * @method FormItem weekPicker($model, array $props = [])
- * @method FormItem monthPicker($model, array $props = [])
- * @method FormItem yearPicker($model, array $props = [])
- * @method FormItem timeSelect($model, array $props = [])
- * @method FormItem timePicker($model, array $props = [])
- * @method FormItem rate($model, array $props = [])
- * @method FormItem colorPicker($model, array $props = [])
- * @method FormItem transfer($model, string $data = null, array $props = [])
- *
  * @package CodeSinging\ElementUiBuilder\Components
  */
 class FormItem extends Component
@@ -79,7 +55,7 @@ class FormItem extends Component
      * @param string|null       $label
      * @param array             $attributes
      */
-    public function __construct($prop = null, string $label = null, array $attributes = [])
+    public function __construct($prop = null, string $label = null, array $attributes = null)
     {
         if (is_array($prop)) {
             parent::__construct($prop);
@@ -90,77 +66,6 @@ class FormItem extends Component
         }
 
         $this->validation = new Validation();
-    }
-
-    /**
-     * Handle dynamic calls to the builder to set attributes or bind a component.
-     *
-     * @param string $name
-     * @param array  $arguments
-     *
-     * @return $this
-     */
-    public function __call($name, $arguments)
-    {
-        $components = [
-            'radio',
-            'radioGroup',
-            'checkbox',
-            'checkboxGroup',
-            'input',
-            'inputNumber',
-            'select',
-            'cascader',
-            'switcher',
-            'slider',
-            'datePicker',
-            'dateTimePicker',
-            'datesPicker',
-            'dateRangePicker',
-            'dateTimeRangePicker',
-            'weekPicker',
-            'monthPicker',
-            'yearPicker',
-            'timeSelect',
-            'timePicker',
-            'rate',
-            'colorPicker',
-            'transfer',
-        ];
-
-        if (in_array($name, $components)) {
-            $this->bindComponent($name, $arguments);
-        } else {
-            parent::__call($name, $arguments);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Bind a component to the form item.
-     *
-     * @param string $name
-     * @param array  $arguments
-     */
-    protected function bindComponent(string $name, array $arguments)
-    {
-        $class = Str::beforeLast(get_class($this), '\\') . '\\' . ucfirst($name);
-
-        $model = array_shift($arguments);
-
-
-        if ($model instanceof Closure) {
-            /** @var Component $component */
-            $component = new $class();
-            $component = call_user_func($model, $component) ?? $component;
-        } elseif ($model instanceof Component) {
-            $component = $model;
-        } else {
-            $component = new $class($model, ...$arguments);
-        }
-
-        $this->add($component);
     }
 
     /**
@@ -214,6 +119,7 @@ class FormItem extends Component
 
     /**
      * Set the default value for the form item.
+     *
      * @param $value
      *
      * @return $this
@@ -229,10 +135,10 @@ class FormItem extends Component
         parent::__build();
 
         $this->store([
-            self::DEFAULT_STORE_KEY. '.'. $this->get('prop') => $this->default,
+            self::DEFAULT_STORE_KEY . '.' . $this->get('prop') => $this->default,
         ]);
 
-        if (!$this->validation->isEmpty()){
+        if (!$this->validation->isEmpty()) {
             $rules = [];
 
             if ($this->validation->when === 'both') {
@@ -245,7 +151,7 @@ class FormItem extends Component
 
             $this->config(compact('rules'));
 
-            $this->set(':rules', $this->configKey('[scene]'));
+            $this->set(':rules', $this->configKey('rules[scene]'));
         }
     }
 }
