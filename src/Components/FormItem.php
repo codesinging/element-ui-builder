9295050374b03b85e1b8f27bd5a9dded767a26ad
codesinging <codesinging@gmail.com>
2020-01-17 10:26:10 +0800

    Add validation to form item.

--- src/Components/FormItem.php
+++ src/Components/FormItem.php
@@ -6,7 +6,9 @@
 
 namespace CodeSinging\ElementUiBuilder\Components;
 
+use Closure;
 use CodeSinging\ElementUiBuilder\Foundation\Component;
+use CodeSinging\ElementUiBuilder\Support\Validation;
 use CodeSinging\Support\Str;
 
 /**
@@ -55,18 +57,39 @@ class FormItem extends Component
     const SIZE_SMALL = 'small';
     const SIZE_MINI = 'mini';
 
+    // Store key for default value.
+    const DEFAULT_STORE_KEY = 'defaultItem';
+
+    /**
+     * The default value.
+     * @var mixed
+     */
+    protected $default;
+
+    /**
+     * The Validate instance.
+     * @var Validation
+     */
+    protected $validation;
+
     /**
      * FormItem constructor.
      *
-     * @param string|null $prop
-     * @param string|null $label
-     * @param array       $props
+     * @param string|array|null $prop
+     * @param string|null       $label
+     * @param array             $attributes
      */
-    public function __construct(string $prop = null, string $label = null, array $props = [])
+    public function __construct($prop = null, string $label = null, array $attributes = [])
     {
-        parent::__construct($props);
-        $prop and $this->set('prop', $prop);
-        $label and $this->set('label', $label);
+        if (is_array($prop)) {
+            parent::__construct($prop);
+        } else {
+            parent::__construct($attributes);
+            $prop and $this->set('prop', $prop);
+            $label and $this->set('label', $label);
+        }
+
+        $this->validation = new Validation();
     }
 
     /**
@@ -127,7 +150,7 @@ class FormItem extends Component
         $model = array_shift($arguments);
 
 
-        if ($model instanceof \Closure) {
+        if ($model instanceof Closure) {
             /** @var Component $component */
             $component = new $class();
             $component = call_user_func($model, $component) ?? $component;
@@ -138,5 +161,95 @@ class FormItem extends Component
         }
 
         $this->add($component);
+    }
+
+    /**
+     * Add validation rule for the form item.
+     *
+     * @param Validation|Closure|array $rule
+     * @param string                   $when
+     *
+     * @return $this
+     */
+    public function validate($rule, string $when = 'both')
+    {
+        if ($rule instanceof Validation) {
+            $this->validation = $rule;
+        } elseif ($rule instanceof Closure) {
+            $this->validation = $rule($this->validation) ?? $this->validation;
+        } elseif (is_array($rule)) {
+            $this->validation->rule($rule);
+        }
+
+        $this->validation->when($when);
+
+        return $this;
+    }
+
+    /**
+     * Add a `add` validation rule for the item.
+     *
+     * @param $rule
+     *
+     * @return $this
+     */
+    public function validateWhenAdd($rule)
+    {
+        $this->validate($rule, 'add');
+        return $this;
+    }
+
+    /**
+     * Add a `edit` validation rule for the item.
+     *
+     * @param $rule
+     *
+     * @return $this
+     */
+    public function validateWhenEdit($rule)
+    {
+        $this->validate($rule, 'edit');
+        return $this;
+    }
+
+    /**
+     * Set the default value for the form item.
+     * @param $value
+     *
+     * @return $this
+     */
+    public function default($value)
+    {
+        $this->default = $value;
+        return $this;
+    }
+
+    protected function __build()
+    {
+        parent::__build();
+
+        $this->store([
+            self::DEFAULT_STORE_KEY. '.'. $this->get('prop') => $this->default,
+        ]);
+
+        if (!$this->validation->isEmpty()){
+            $rules = [];
+
+            if ($this->validation->when === 'both') {
+                $rules['add'] = $rules['edit'] = $this->validation->rules();
+            } elseif ($this->validation->when === 'add') {
+                $rules['add'] = $this->validation->rules();
+            } elseif ($this->validation->when === 'edit') {
+                $rules['edit'] = $this->validation->rules();
+            }
+
+            $name = $this->buildId('rules.rule');
+
+            $this->store([
+                $name => $rules,
+            ]);
+
+            $this->set(':rules', $name.'[scene]');
+        }
     }
 }
